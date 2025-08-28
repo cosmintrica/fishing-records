05609037c43d2e1cf91ff1f0b5b7fb502e066d8a
cosmintrica <cosmintrricaa@gmail.com>
2025-08-28 23:13:02 +0300

    Create firebaseAdmin.ts

---
+++ api/firebaseAdmin.ts
@@ -0,0 +1,19 @@
+// api/firebaseAdmin.ts
+import * as admin from 'firebase-admin';
+
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+const projectId = process.env.FIREBASE_PROJECT_ID;
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId,
+      clientEmail,
+      privateKey,
+    }),
+  });
+}
+
+export const adminAuth = admin.auth();
+export const adminDb = admin.firestore();
